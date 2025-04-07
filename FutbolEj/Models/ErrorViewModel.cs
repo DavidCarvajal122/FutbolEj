740c5abaf3a4ac60cead55b10942cff6ba35e313
User <User@DAVID>
2025-04-07 18:31:46 -0500

    Agregar archivos de proyecto.

---
+++ FutbolEj/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FutbolEj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
